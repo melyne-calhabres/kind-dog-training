1aaf0a3f99d6e0e11df97fd9c178ea0007320666
Rockus33 <anthony@morpheus.agency>
2026-05-26 15:39:41 +0200

    corrections SEO + favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://mondomaine.fr',
+  site: 'https://kind-dog-training.fr',
   output: 'static',
   integrations: [],
   image: {
